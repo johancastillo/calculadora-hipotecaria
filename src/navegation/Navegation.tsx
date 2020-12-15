72d0f9a2e3c9ea9cf13f4204bbe0328ae3fdef6f
Johan Castillo <jcjohan2707@gmail.com>
2020-12-15 15:23:46 -0400

    Created components

---
+++ src/navegation/Navegation.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navegation = () => (
+    <h1>Navegation</h1>
+);
+
+export default Navegation;
---
+++ src/table/Table.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Table = () => (
+    <h1>Teble</h1>
+);
+
+export default Table;
